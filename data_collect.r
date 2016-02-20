f7a27b40dafccdb8bc00c01ab5ee9a2bef84dc13
mrobinson72 <mrobinson72@gatech.edu>
2016-02-19 23:45:17 -0600

    script updates

--- data_collect.r
+++ data_collect.r
@@ -21,16 +21,3 @@ for (file in data){
 }
 write.csv(ex_rate,file='oecd')
 
-test_set <- subset(ex_rate,as.Date(ex_rate$Date)>"2014-01-01")
-train_set <- subset(ex_rate,as.Date(ex_rate$Date)<="2014-01-01")
-test_set <- test_set[,c('Exchange_fut', "Exchange", "Infl_d", "Yield_d", "Int_d", "PrimeRate_d", "GDPG_d",
-               "Infl_f", "Int_f", "GDPG_f", "Country.y")]
-train_set <- train_set[,c('Exchange_fut', "Exchange", "Infl_d", "Yield_d", "Int_d", "PrimeRate_d", "GDPG_d",
-               "Infl_f", "Int_f", "GDPG_f", "Country.y")]
-
-regmodel <- lm(Exchange_fut ~ Exchange + Infl_d + Yield_d + Int_d + PrimeRate_d + GDPG_d  +
-               Infl_f  + Int_f  + GDPG_f  + Country.y ,data = train_set)
-
-summary(regmodel)
-
-ypred <- predict.lm(regmodel,test_set)
